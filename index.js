10652fdd053cd3a9e0d53ef4e870b01958b4ce87
Kishore Chandrasekharan <kishore28m@gmail.com>
2019-04-18 20:53:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 function shout(string) {
   return string.toUpperCase();
 }
-
-function whisper(string) {
-  return whisper.toLowerCase();
-}
-
-function logShout(string){
-  console.log(string.toUpperCase();
-}
-
-function logWhisper(string) {
-  console.log(whisper.toLowerCase());
-}
