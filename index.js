6c24024b312e12a3e054544e19c7f355a6222a31
Kishore Chandrasekharan <kishore28m@gmail.com>
2019-04-18 21:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ function logShout(string){
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  /*var uppercase = string.toUpperCase();
+  var lowercase = string.toLowerCase();
+
+  if (string === lowercase) {
+    return "I can\'t hear you!";
+  }
+  else if (string === uppercase) {
+    return "YES INDEED!"
+  }
+  else {
+    
+  }*/
+  switch (string) {
+    case (string.toUpperCase()):
+      return "YES INDEED!"
+      break;
+    case (string.toLowerCase()):
+      return "I can\'t hear you!";
+      break;
+    case ("I love you, Grandma."):
+      return "I love you, too.";
+      break;
+    default:
+      return "please enter a valid text";
+  }
+}
